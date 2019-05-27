ac3657ef18f7daa5b9c5c886a6a56daf9d6e099b
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-05-27 12:27:38 +0600

    added a component to handle rendering icons

--- r_weather/src/components/DisplayWeather.tsx
+++ r_weather/src/components/DisplayWeather.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import {Component , Fragment } from 'react';
 
 import {WeatherData} from '../types/types';
-
+import moment from 'moment';
+import ShowIcons from './ShowIcons';
 
 interface Props{
-    weatherData?:WeatherData
+    weatherData?:WeatherData,
+    img?:string
 }
 interface State{
 
@@ -16,17 +18,33 @@ export default class DisplayWeather extends Component<Props , State> {
 
     render(){
         const {weatherData} = this.props;
+        
         if(weatherData){
+            const {city , country , description ,icon , realfeel , sunrise ,sunset , temp , ts } = weatherData;
             return(
-                <Fragment>
-                    <h3>{weatherData.city}, {' '}
-                        {weatherData.country}
-                    </h3>
-                    <h4>
-                        {weatherData.temp}
-                    </h4>
-                    
-                </Fragment>
+                 <div className="hero_container">
+                 <img src={this.props.img } alt="location images" />
+                     <div className="text-block">
+                         <div className="banner">
+                             <p> { city +","+ country}</p>
+                             <p> { moment(ts *1000).format('dddd, MMM Do') }</p>
+                         </div>
+                         <div className="main">
+                             <div className="main__icon"> 
+                                 <ShowIcons icon={icon} /> 
+                                 <p> {description }</p>
+                             </div>
+                             <div className="temp">
+                                 <p className="temp_val" >{ temp }<span className="wi wi-celsius"></span>
+                                 </p>
+                                 <p className="realfeel"><i className="wi wi-thermometer">{ realfeel}<span className="wi wi-celsius"></span></i></p>
+                                 <p className="rs"><i className="wi wi-sunrise">{ sunrise }</i> <i  className="wi wi-sunset">{ sunset }</i> </p>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+
+
             );
         }
         
