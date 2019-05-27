88ecf8c8411d9bb59f090b8d061ebfae3b162ad7
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-05-27 18:39:38 +0600

    splited code and cleaned app.tsx

--- r_weather/src/App.tsx
+++ r_weather/src/App.tsx
@@ -2,19 +2,15 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 
-import Unsplash from 'unsplash-js';
 
 import './components/InputQuery';
 import Navigation from './components/Navigation';
-import InputQuery from './components/InputQuery';
 import DisplayWeather from './components/DisplayWeather';
 import DailyWeather from './components/DailyWeather';
 import Graph from './components/Graph';
 
 
-import {weather__APIkey,W_Current_url ,unsplash_key , unsplash_secret ,daily_url,hourly_url } from './config/configs'
-import { WeatherData, Geolocation, dailyData, hourlyData } from './types/types';
-import moment from 'moment';
+import { WeatherData, dailyData, hourlyData } from './types/types';
 
 
 import {getCurrentUserLocation , getCurrentWeather, getPicture, getdailyWeather, gethourlyWeather} from './helpers/helpers';
@@ -47,152 +43,6 @@ class App extends React.Component{
     term:''
   }
 
-  // getCurrentUserLocation =(): Promise<Geolocation>=>{
-  //   return new Promise((resolve , reject) =>{
-  //     if(!navigator.geolocation){
-  //       reject('Geolocation is not Working');
-
-  //     }else {
-  //       console.log('getting location/...');
-  //       navigator.geolocation.watchPosition(position =>{
-          
-  //         resolve({
-  //           latitude:position.coords.latitude,
-  //           longitude:position.coords.longitude
-  //         });
-  //       } , err =>{
-  //         reject(`Cant get current Location :${err.message}`);
-  //       })
-        
-  //     }
-  //   })
-  // }
-
-  // getCurrentWeather = async(geolocation : Geolocation)  =>{
-  //   const {latitude, longitude} = geolocation;
-  //   this.setState({
-  //     lat:latitude,
-  //     lon:longitude
-  //   })
-  //   console.log(geolocation);
-    
-  //   let url = `${W_Current_url}lat=${latitude}&lon=${longitude}&key=${weather__APIkey}`;
-  //   console.log(url);
-
-  //  await fetch(url)
-  //     .then(res => res.json())
-  //     .then(data =>{
-  //       let obj = {
-  //         city:data.data[0].city_name,
-  //         country: data.data[0].country_code,
-  //         des:data.data[0].weather.description,
-  //         temp:data.data[0].temp,
-  //         realfeel:data.data[0].app_temp,
-  //         icon:data.data[0].weather.code,
-  //         sunrise:data.data[0].sunrise,
-  //         sunset:data.data[0].sunset,
-  //         ts:data.data[0].ts,
-  //         lat:data.data[0].lat,
-  //         lon:data.data[0].lon
-
-  //       };
-  //       return  {
-  //         city:obj.city,
-  //         country:obj.country,
-  //         description:obj.des,
-  //         temp:obj.temp,
-  //         realfeel:obj.realfeel,
-  //         icon:obj.icon,
-  //         sunrise:obj.sunrise,
-  //         sunset:obj.sunset,
-  //         ts:obj.ts,
-  //         lat:obj.lat, 
-  //         lon:obj.lon
-  //       }
-        
-        
-  //     })
-  //     .then((term)=>{
-  //       this.setState({
-  //         weatherData:term,
-  //         term: term.description
-  //       })
-  //     })
-
-  //     .catch(err => console.log(err));
-
-      
-      
-  // }
-  // getPicture=  (query : string ):void =>{
-    
-  //   unsplash.search.photos(query+" nature", 1 , 10 )
-  //   .then(res => res.json())
-  //   .then(json => {
-  //     console.log(json);
-  //     this.setState({
-  //       loading:false,
-  //       img:json.results[3].urls.regular,
-  //     })
-  //   })
-  //   .catch((err)=>{
-  //     console.log(err);
-  //     // this.setState({
-  //     //   loading:false,
-  //     //   error_picture:"Broken Search ... Please type again",
-  //     // })
-  //   })
-  // }
-
-  // getdailyWeather =  ():void  =>{
-  //   let url = `${daily_url}lat=${this.state.lat}&lon=${this.state.lon}&days=5&key=${weather__APIkey}`;
-    
-  //   fetch(url)
-  //   .then(res => res.json())
-  //   .then(data =>{
-      
-  //     return data.data.map((w : any)=>{
-  //       console.log(w.valid_data);
-        
-  //       return {
-  //         icon:w.weather.code,
-  //         description:w.weather.description,
-  //         hi_temp:w.max_temp,
-  //         lo_temp:w.min_temp,
-  //         ts:w.valid_date
-  //       }
-  //     })
-  //   })
-  //   .then((arr)=>{
-  //     console.log(arr);
-      
-  //     this.setState({
-  //       dailyData:arr
-  //     })
-  //   })
-  //   .catch(err => console.log(err));
-  // }
-  // gethourlyWeather=():void =>{
-  //   let url = `${hourly_url}lat=${this.state.lat}&lon=${this.state.lon}&hours=12&key=${weather__APIkey}`;
-    
-  //   fetch(url)
-  //   .then(res => res.json())
-  //   .then(data =>{
-  //     return data.data.map((d:any )=>{
-  //       let date = moment(d.ts *1000 ).format('h:mm A');
-  //       return {
-  //         Time: date ,
-  //         Temp:d.temp 
-  //       }
-  //     })
-      
-  //   })
-  //   .then((arr ) => this.setState ({
-  //     loading: false ,
-  //     hourlyData:arr
-  //   }))
-  //   .catch(err => console.log(err));
-  // }
 
 
 
