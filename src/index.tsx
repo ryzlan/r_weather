1872d1f9ad7241f2bce27f7e65e52771fe519dbc
Ruzlan Karim <ruzlankarim2@gmail.com>
2019-05-28 20:13:30 +0600

    FIXED PWA SERVICE WORKER ISSUE

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './weather-icon/css/weather-icons.min.css';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App  />, document.getElementById('root'));
 
 
-serviceWorker.register();
+
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,18 +1,6 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
 
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
 
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://bit.ly/CRA-PWA
-
-// Flag for enabling cache in production
-
-const cacheName = 'v2';
+const cacheName = 'v1: :';
 
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
@@ -64,7 +52,7 @@ export function register(config?: Config) {
       }
     });
 
-    window.addEventListener('install', e => {
+    window.addEventListener('install', (e :any ) => {
       console.log('Service Worker: Installed');
     });
 
